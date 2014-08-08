09ed61ba4e7b219876fd393cba626ea5b095e63e
Your Name <your.email@example.com>
2014-08-08 19:39:56 +0300

    fixed articles_controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,63 +1,53 @@
 class ArticlesController < ApplicationController
- def new
- 	@article = Article.new
- end
+  def new
+   	@article = Article.new
+  end
 
- def create
- @article = Article.new(article_params)
+  def create
+    @article = Article.new(article_params)
 
- if @article.save
- redirect_to @article
- else
-    render 'new'
- end
-end 
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
 
- private
- def article_params
- params.require(:article).permit(:title, :text)
- end
-
- def show
- @article = Article.find(params[:id])
- end
+  def show
+    @article = Article.find(params[:id])
+  end
 
   def index
-  @articles = Article.all
- end
-
- def edit
- 	@article = Article.find(params[:id])
- end
-
- def update
-  @article = Article.find(params[:id])
- 
-  if @article.update(article_params)
-    redirect_to @article
-  else
-    render 'edit'
+    @articles = Article.all
   end
-end
- 
-private
-  def article_params
-    params.require(:article).permit(:title, :text)
+
+  def edit
+   	@article = Article.find(params[:id])
   end
-  
+
+	def update
+	  @article = Article.find(params[:id])
+	 
+	  if @article.update(article_params)
+	    redirect_to @article
+	  else
+	    render 'edit'
+	  end
+	end
+   
   def update
-  @article = Article.find(params[:id])
- 
-  if @article.update(article_params)
-    redirect_to @article
-  else
-    render 'edit'
+		@article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
-end
- 
-private
+
+  private
   def article_params
     params.require(:article).permit(:title, :text)
   end
 end
+ 
 
